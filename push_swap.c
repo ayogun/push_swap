f1e4a4fdbc875cdc0ae67a7ce9cd54a0bc354fb7
A.YOGUN <yogun@student.42heilbronn.de>
2022-05-30 20:06:16 +0200

    Update push_swap.c

--- push_swap.c
+++ push_swap.c
@@ -101,10 +101,14 @@ void	ft_add_back(t_stack **stack, t_stack *stack_new)
 t_stack	*ft_stack_new(long content)
 {
 	t_stack	*new;
-
+	
+	//We are creating neccessary free memory space in the RAM.
 	new = (t_stack *)malloc(sizeof(t_stack));
+	//We are protecting our memory allocation to avoid potential leakages.
 	if (new == NULL)
 		return (NULL);
+	//In case of everything is okay, we put the argument ,which come to the function with the name of "content", into nbr value-
+	//which is a member of the "new" node. Next is current pointing the NULL. We will link that list into our stack with ft_add_back.
 	new->nbr = content;
 	new->next = NULL;
 	return (new);
@@ -121,16 +125,21 @@ int	ft_atoi(const char *str)
 	isnegative = 0;
 	while ((str[i] <= 13 && str[i] >= 9) || str[i] == 32)
 		i++;
+	//We check whether the value is negative or not
 	if (str[i] == '-')
 		isnegative = 1;
 	if (str[i] == '+' || str[i] == '-')
 		i++;
+	//We are making an ASCII check just in case. If everything is okay we make necessary math operations to turn our string-
+	//into an integer value. Our purpose to turn the value into integer, is the neccessity of doing math operations in arguments.
 	while (str[i] >= 48 && str[i] <= 57)
 	{
 		res *= 10;
 		res += ((int)str[i] - 48);
 		i++;
 	}
+	//If our isnegative value gives as '1' which means "true", it will lead us into our if condition and append a negative sign-
+	//to the integer value.
 	if (isnegative)
 		return (-res);
 	else
@@ -144,13 +153,18 @@ void	list_args(char **argv, t_stack **stack_a)
 	i = 1;
 	while (argv[i] != NULL)
 	{
+			//While arguments are valid, we start to add them into stack here one by one with while loop.
+			//Atoi takes the number and turn it into an integer value where we can make math operations.
+			//With stack new we create a new node for the current argument without linking it to list.
+			//We make linking stage in ft_add_back call.
 			ft_add_back(stack_a, ft_stack_new(ft_atoi(argv[i])));
 		i++;
 	}
 }
 
 void	ft_error(void)
-{
+{	
+	//Here an error message is being printed out
 	write (2, "Error\n", 6);
 	exit(1);
 }
@@ -166,6 +180,7 @@ void	alpha_check(char **argv)
 		j = 0;
 		while ((argv[i][j]) != '\0')
 		{
+			//Here arguments are send to make ASCII check regarding the alphanumeric values
 			if (ft_isalpha(argv[i][j]))
 				ft_error();
 			j++;
@@ -176,6 +191,7 @@ void	alpha_check(char **argv)
 
 int	check_args(char **argv)
 {
+	//With this function it is checked whether arguments include alfanumeric or not
 	alpha_check(argv);
 	if (!check_error(argv, 1, 0))
 		return (false);
